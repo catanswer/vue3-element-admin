6229d58393a50f06fea020e2e9ab064300a0fafe
catanswer <564029918@qq.com>
2021-03-27 18:03:44 +0800

    init: page file

---
+++ src/views/home/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-container">
+    <smart-layout>
+      <template #top>
+        <div style="height: 100px; background: red"></div>
+      </template>
+      <div style="height: 100%;background: blue"></div>
+      <template #bottom>
+        <div style="height: 100px; background: red"></div>
+      </template>
+    </smart-layout>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/setting/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-container">
+    <smart-layout>
+      asdad
+    </smart-layout>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang='scss' scoped>
+</style>

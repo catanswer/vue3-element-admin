553e0ba81fb6fc02e3e61a007d41113570e9286c
catanswer <564029918@qq.com>
2021-03-27 18:03:44 +0800

    init: page file

---
+++ src/views/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-scrollbar>
+    <div class="page-container">
+      <div style="height: 1000px; ">123213</div>
+    </div>
+  </el-scrollbar>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>

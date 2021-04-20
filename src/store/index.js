b27201f6b1ea08a12a0647ce0eeaa86c9137b0d2
catanswer <564029918@qq.com>
2021-04-20 15:18:13 +0800

    update: using vuex-persist plugin

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,24 @@
 import { createStore } from 'vuex'
+import VuexPersistence from 'vuex-persist'
 import app from './modules/app'
+
+const vuexLocal = new VuexPersistence({
+  key: 'sideMenuCollapsed',
+  modules: ['app'],
+  reducer: state => ({
+    app: {
+      sideMenu: {
+        collapsed: state.app.sideMenu.collapsed
+      }
+    }
+  })
+})
 
 const store = createStore({
   modules: {
     app
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
 
 export default store
